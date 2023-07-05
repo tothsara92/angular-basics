a980eb92da5eac05ce16d80a499b0500d0bded5f
tothsara <tothsara92@gmail.com>
2023-07-05 17:11:30 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alapok';
+
+  isInMove = false;
+  xPosition = 0;
+  yPosition = 0;
+
+  boxDragStart() {
+    this.isInMove = true;
+  }
+
+  boxDragEnd() {
+    this.isInMove = false;
+  }
+
+  boxDrag(event: any) {
+    if(this.isInMove) {
+      this.xPosition = event.clientX - 400 / 2;
+      this.yPosition = event.clientY - 150 / 2;
+    }
+  }
 }
