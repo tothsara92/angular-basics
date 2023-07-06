0e9cb374944fb14927aa4246b07566d26d036010
tothsara <tothsara92@gmail.com>
2023-07-06 21:07:37 +0200

    change to fetching data

--- sablon-muveletek/src/app/app.component.ts
+++ sablon-muveletek/src/app/app.component.ts
@@ -1,28 +1,56 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  elements = [
-    "Lorem dshkhgkjshgkdshgkdsghkdshg",
-    "sdjkdshgjkdshgkjsg skdhkshgjksgkhdskghjkshgjksdkgkg",
-    "Helllookkaaa",
-  ];
+export class AppComponent implements OnInit {
+  elements: string[] = [];
+
+  inProgress = false;
+
+  ngOnInit(): void {
+    this.fetchElements();
+  }
+
+  fetchElements() {
+    this.inProgress = true;
+    fetch("https://kodbazis.hu/api/cimek")
+      .then(res => res.json())
+      .then(titles => {
+        this.elements = titles;
+      })
+      .catch(() => {
+        alert("Hiba");
+      })
+      .finally(() => {
+        this.inProgress = false;
+      })
+  }
+
+  deleteTitle(i: number) {
+    this.inProgress = true;
+    fetch("https://kodbazis.hu/api/cimek/" + i, {method: "DELETE"})
+      .then(() => {
+        this.fetchElements();
+      });
+  }
 
   formSubmit(event: any) {
     event.preventDefault();
 
     const inputTitle = event.target.elements.title.value;
+    
+    this.inProgress = true;
 
-    this.elements.push(inputTitle);
+    fetch("https://kodbazis.hu/api/cimek", {method: "POST", body: JSON.stringify({
+      cim: inputTitle
+    })})
+      .then(() => {
+        this.fetchElements();
+      });
 
     event.target.reset();
-  }
-
-  deleteTitle(i: number) {
-    this.elements.splice(i, 1);
   }
 }
